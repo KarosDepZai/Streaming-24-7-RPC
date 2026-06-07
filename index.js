aff862b07ea6571c27b2ffc8fc91c80cbb5deae1
Karos <kunidepzai@gmail.com>
2026-06-07 16:37:27 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,66 @@
+
+
 const Discord = require("discord.js-selfbot-v13");
 const client = new Discord.Client({
-  readyStatus: false,
-  checkUpdate: false,
-});
+  readyStatus: false,
+  checkUpdate: false,
+}); 
 
 const keepAlive = require("./server.js");
-keepAlive();
+keepAlive(); 
 
 function formatTime() {
-  //Credits to himika#0001 and never#0001
-  const date = new Date();
-  const options = {
-    timeZone: "America/New_York", //https://www.zeitverschiebung.net/en/ and find your city and enter here
-    hour12: true,
-    hour: "numeric",
-    minute: "numeric",
-  };
-  return new Intl.DateTimeFormat("en-US", options).format(date);
-}
+  //Credits to himika#0001 and never#0001
+  const date = new Date();
+  const options = {
+    timeZone: "America/New_York", //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+  };
+  return new Intl.DateTimeFormat("en-US", options).format(date);
+} 
 
 client.on("ready", async () => {
-  console.clear();
-  console.log(`${client.user.tag} - rich presence started!`);
+  console.clear();
+  console.log(`${client.user.tag} - rich presence started!`); 
 
-  const r = new Discord.RichPresence(client)
-    .setApplicationId("1505569255847100546")
-    .setType("PLAYING"); //STREAMING,WATCHING,LISTENING,...
-    .setURL("https://www.youtube.com/watch?v=oHg5SJYRHA0") //Must be a youtube video link
-    .setState("your state")
-    .setName("ᖽᐸａ𝑅𝐎𝕊 ☔?")
-    .setDetails(`Captivates Me [${formatTime()}]`)
-    .setStartTimestamp(Date.now())
-    .setAssetsLargeImage(
-      "https://media.discordapp.net/attachments/1430232655802138734/1430232675104329779/image0.png?ex=68f9077a&is=68f7b5fa&hm=b7b4e4f909e06551333361f7f5f923e8e4ce3bf6d2efefa5e6a3d074459e5b49&=&format=webp&quality=lossless",
-    ) //You can put links in tenor or discord and etc.
-    .setAssetsLargeText("I have fallen for you") //Text when you hover the Large image
-    .setAssetsSmallImage(
-      "https://media.discordapp.net/attachments/1430232655802138734/1430232703911071824/heart-beating-heart.png?ex=68f90781&is=68f7b601&hm=e0e6f7e840f4d80bcd0569dae8460785b0d2ba5095e4a177772c1531a50c87a9&=&format=webp&quality=lossless",
-    ) //You can put links in tenor or discord and etc.
-    .setAssetsSmallText("Im obsessed with you") //Text when you hover the Small image
-    .addButton("Portfolio", "https://e-z.bio/developer")
-    .addButton(
-      "Subscribe",
-      "https://www.youtube.com/@Jenna_YTthegoat",
-    );
+  const r = new Discord.RichPresence(client)
+    .setApplicationId("1505569255847100546")
+    .setType("PLAYING"); //STREAMING,WATCHING,LISTENING,...
+    .setURL("https://www.youtube.com/watch?v=oHg5SJYRHA0") //Must be a youtube video link
+    .setState("No Reply = Sleep 😴, Play Game 🎮")
+    .setName("ᖽᐸａ𝑅𝐎𝕊 ☔?")
+    .setDetails(`Tôi đang Street [${formatTime()}]`)
+    .setStartTimestamp(Date.now())
+    .setAssetsLargeImage(
+      "https://media.tenor.com/aYsNJR1rUpIAAAAM/gojo.gif",
+    ) //You can put links in tenor or discord and etc.
+    .setAssetsLargeText("I have fallen for you") //Text when you hover the Large image
+    .setAssetsSmallImage(
+      "https://media.tenor.com/aYsNJR1rUpIAAAAM/gojo.gif",
+    ) //You can put links in tenor or discord and etc.
+    .setAssetsSmallText("Im obsessed with you") //Text when you hover the Small image
+    .addButton("My Profile", "https://karosdepzai.vercel.app")
+    .addButton(
+      "My Discord",
+      "https://discord.gg/Gju793PgHT",
+    ); 
 
-  client.user.setActivity(r);
-  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
+  client.user.setActivity(r);
+  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline 
 
-  let prevTime = null;
-  setInterval(() => {
-    const newTime = formatTime();
-    if (newTime !== prevTime) {
-      const newDetails = `Haizz [${newTime}]`;
-      r.setDetails(newDetails);
-      client.user.setActivity(r);
-      prevTime = newTime;
-    }
-  }, 1000); // Update every second
-});
+  let prevTime = null;
+  setInterval(() => {
+    const newTime = formatTime();
+    if (newTime !== prevTime) {
+      const newDetails = `Bâu giờ đang là [${newTime}]`;
+      r.setDetails(newDetails);
+      client.user.setActivity(r);
+      prevTime = newTime;
+    }
+  }, 1000); // Update every second
+}); 
 
 const mySecret = process.env["TOKEN"];
 client.login(mySecret);
