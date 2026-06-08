c37cb7ef5a3ee9f80ddf3ea91caf7b82300daeeb
Karos <kunidepzai@gmail.com>
2026-06-08 15:05:30 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const keepAlive = require("./server.js");
 keepAlive(); 
 
 function formatTime() {
-  //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: "Asia/Ho_Chi_Minh", // Đã sửa thành múi giờ chuẩn quốc tế của Việt Nam
+    timeZone: "Asia/Ho_Chi_Minh", 
     hour12: true,
     hour: "numeric",
     minute: "numeric",
@@ -23,6 +22,11 @@ client.on("ready", async () => {
   console.clear();
   console.log(`${client.user.tag} - rich presence started!`); 
 
+  const fake_tieng_truoc = 999999;  // Set số bất kì để hiện số Giờ bạn muốn
+  const fake_phut_truoc  = 999999; // Set số bất kì để hiện số Phút bạn muốn
+  
+  const thoiGianFake = Date.now() - (fake_tieng_truoc * 60 * 60 * 1000) - (fake_phut_truoc * 60 * 1000);
+
   const r = new Discord.RichPresence(client)
     .setApplicationId("1505569255847100546")
     .setType("PLAYING")
@@ -30,7 +34,9 @@ client.on("ready", async () => {
     .setState("ɴᴏ ʀᴇᴘʟʏ = sʟᴇᴇᴘ 😪, ᴘʟᴀʏ ɢᴀᴍᴇ 🎮")
     .setName("ᖽᐸａ𝑅Ｏ𝕊 ❄?")
     .setDetails(`Tôi đang Street [${formatTime()}]`)
-    .setStartTimestamp(Date.now())
+    
+    .setStartTimestamp(thoiGianFake)
+
     .setAssetsLargeImage("1506663140522201089") 
     .setAssetsLargeText("I have fallen for you") 
     .setAssetsSmallImage("1506884112722690048") 
@@ -39,18 +45,18 @@ client.on("ready", async () => {
     .addButton("‧₊˚.⊹xDiscord✧.*", "https://dsc.gg/congdongviet");
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); // dnd, online, idle, offline 
+  client.user.setPresence({ status: "dnd" }); 
 
   let prevTime = null;
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `ßα̂ყ ցισ̛̀ ɬὰ [${newTime}] 🔥`;
+      const newDetails = `ßα̂ყ ցισ̛̀ ɬὰ [${newTime}] 🔥`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
     }
-  }, 1000); // Cập nhật mỗi giây
+  }, 1000); 
 }); 
 
 const mySecret = process.env["TOKEN"];
